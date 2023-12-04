cc8c4891c6387b2763967d00f7c35a68d5dc9ef8
cikeV <151319654+cikeV@users.noreply.github.com>
2023-12-05 07:06:38 +0800

    Update MOJSN.lua

--- MOJSN.lua
+++ MOJSN.lua
@@ -1,3 +1,10 @@
+local args = {
+    [1] = "刺 客 免 费 版",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
 msg = Instance.new("Message")
 msg.Parent = game.Workspace
 msg.Text = "刺 客 V 1"
@@ -12,34 +19,29 @@ wait(3)
 msg:remove()
 wait(1)
 
-TL = Instance.new("TextLabel", Instance.new("ScreenGui", game.CoreGui))
-TL.BackgroundTransparency = 1
-TL.Size = UDim2.new(1, 1, 1, 1)
-TL.Text = "刺 客 V 1"
-
-local SFGui = Instance.new("ScreenGui")
+local CKGui = Instance.new("ScreenGui")
 local Game = Instance.new("TextLabel")
 local Geame = Instance.new("TextLabel")
 local Main = Instance.new("Frame")
 local Switch = Instance.new("TextButton")
 local SwitchCorner = Instance.new("UICorner")
 local Name = Instance.new("TextLabel")
-local SF = Instance.new("TextButton")
-local SF2 = Instance.new("TextButton")
-local SF3 = Instance.new("TextButton")
-local SF4 = Instance.new("TextButton")
-local SF5 = Instance.new("TextButton")
-local SF6 = Instance.new("TextButton")
-local SF7 = Instance.new("TextButton")
-local SF8 = Instance.new("TextButton")
-local SF9 = Instance.new("TextButton")
+local CK = Instance.new("TextButton")
+local CK2 = Instance.new("TextButton")
+local CK3 = Instance.new("TextButton")
+local CK4 = Instance.new("TextButton")
+local CK5 = Instance.new("TextButton")
+local CK6 = Instance.new("TextButton")
+local CK7 = Instance.new("TextButton")
+local CK8 = Instance.new("TextButton")
+local CK9 = Instance.new("TextButton")
 
-SFGui.Name = "WuDy"
-SFGui.Parent = game.CoreGui
-SFGui.ResetOnSpawn = false
+CKGui.Name = "WuDy"
+CKGui.Parent = game.CoreGui
+CKGui.ResetOnSpawn = false
 
 Main.Name = "UI"
-Main.Parent = SFGui
+Main.Parent = CKGui
 Main.Active = true
 Main.BackgroundColor3 = Color3.new(0, 0, 0)
 Main.BackgroundTransparency = 0.20000000298023
@@ -48,7 +50,7 @@ Main.Size = UDim2.new(0, 680, 0, 320)
 Main.Draggable = true
 
 Switch.Name = "Switch"
-Switch.Parent = SFGui
+Switch.Parent = CKGui
 Switch.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
 Switch.BackgroundTransparency = 0.5
 Switch.Position = UDim2.new(0.2, 0, 0, 0)
@@ -105,146 +107,146 @@ Geame.Text = "(点击文字执行)"
 Geame.TextColor3 = Color3.fromRGB(255, 255, 255)
 Geame.TextSize = 27.000
 
-SF.Name = "SF"
-SF.Parent = Main
-SF.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
-SF.BackgroundTransparency = 0.800
-SF.BorderSizePixel = 0
-SF.Position = UDim2.new(0.000000002, 0, 0.2235255, 0)
-SF.Size = UDim2.new(0, 220, 0, 20)
-SF.Font = Enum.Font.Arial
-SF.Text = "极速传奇"
-SF.TextColor3 = Color3.fromRGB(255, 255, 255)
-SF.TextSize = 14.000
-SF.MouseButton1Click:Connect(function()
+CK.Name = "CK"
+CK.Parent = Main
+CK.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
+CK.BackgroundTransparency = 0.800
+CK.BorderSizePixel = 0
+CK.Position = UDim2.new(0.000000002, 0, 0.2235255, 0)
+CK.Size = UDim2.new(0, 220, 0, 20)
+CK.Font = Enum.Font.Arial
+CK.Text = "极速传奇"
+CK.TextColor3 = Color3.fromRGB(255, 255, 255)
+CK.TextSize = 14.000
+CK.MouseButton1Click:Connect(function()
 game:GetService("StarterGui"):SetCore("SendNotification",{ Title = "极速传奇"; Text ="加载中…"; Duration = 4; });
 loadstring(game:HttpGet("https://raw.github.com/cikeV/-/main/%E6%9E%81%E9%80%9F.lua"))()
 end)
 
-SF2.Name = "SF"
-SF2.Parent = Main
-SF2.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
-SF2.BackgroundTransparency = 0.800
-SF2.BorderSizePixel = 0
-SF2.Position = UDim2.new(0.33, 0, 0.2235, 0)--2高 3宽
-SF2.Size = UDim2.new(0, 220, 0, 22)
-SF2.Font = Enum.Font.Arial
-SF2.Text = "监狱人生"
-SF2.TextColor3 = Color3.fromRGB(255, 255, 255)
-SF2.TextSize = 14.000
-SF2.MouseButton1Click:Connect(function()
+CK2.Name = "CK"
+CK2.Parent = Main
+CK2.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
+CK2.BackgroundTransparency = 0.800
+CK2.BorderSizePixel = 0
+CK2.Position = UDim2.new(0.33, 0, 0.2235, 0)--2高 3宽
+CK2.Size = UDim2.new(0, 220, 0, 22)
+CK2.Font = Enum.Font.Arial
+CK2.Text = "监狱人生"
+CK2.TextColor3 = Color3.fromRGB(255, 255, 255)
+CK2.TextSize = 14.000
+CK2.MouseButton1Click:Connect(function()
 game:GetService("StarterGui"):SetCore("SendNotification",{ Title = "监狱人生"; Text ="加载中…"; Duration = 4; });
 loadstring(game:HttpGet("https://raw.github.com/cikeV/-/main/%E7%9B%91%E7%8B%B1.lua"))()
 end)
 
-SF3.Name = "SF"
-SF3.Parent = Main
-SF3.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
-SF3.BackgroundTransparency = 0.800
-SF3.BorderSizePixel = 0
-SF3.Position = UDim2.new(0.67, 0, 0.2235, 0)
-SF3.Size = UDim2.new(0, 220, 0, 22)
-SF3.Font = Enum.Font.Arial
-SF3.Text = "DOORS"
-SF3.TextColor3 = Color3.fromRGB(255, 255, 255)
-SF3.TextSize = 12.000
-SF3.MouseButton1Click:Connect(function()
-game:GetService("StarterGui"):SetCore("SendNotification",{ Title = "DOORS"; Text ="加载中…"; Duration = 4; });
-loadstring(game:HttpGet("https://raw.github.com/cikeV/-/main/DO.lua"))()
+CK3.Name = "CK"
+CK3.Parent = Main
+CK3.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
+CK3.BackgroundTransparency = 0.800
+CK3.BorderSizePixel = 0
+CK3.Position = UDim2.new(0.67, 0, 0.2235, 0)
+CK3.Size = UDim2.new(0, 220, 0, 22)
+CK3.Font = Enum.Font.Arial
+CK3.Text = "Blox Fruits"
+CK3.TextColor3 = Color3.fromRGB(255, 255, 255)
+CK3.TextSize = 12.000
+CK3.MouseButton1Click:Connect(function()
+game:GetService("StarterGui"):SetCore("SendNotification",{ Title = "Blox Fruits"; Text ="加载中…"; Duration = 4; });
+loadstring(game:HttpGet("https://raw.github.com/cikeV/-/main/BF.lua"))()
 end)
 
-SF4.Name = "SF"
-SF4.Parent = Main
-SF4.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
-SF4.BackgroundTransparency = 0.800
-SF4.BorderSizePixel = 0
-SF4.Position = UDim2.new(0.000000002, 0, 0.3, 0)
-SF4.Size = UDim2.new(0, 220, 0, 22)
-SF4.Font = Enum.Font.Arial
-SF4.Text = "忍者传奇"
-SF4.TextColor3 = Color3.fromRGB(255, 255, 255)
-SF4.TextSize = 14.000
-SF4.MouseButton1Click:Connect(function()
+CK4.Name = "CK"
+CK4.Parent = Main
+CK4.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
+CK4.BackgroundTransparency = 0.800
+CK4.BorderSizePixel = 0
+CK4.Position = UDim2.new(0.000000002, 0, 0.3, 0)
+CK4.Size = UDim2.new(0, 220, 0, 22)
+CK4.Font = Enum.Font.Arial
+CK4.Text = "忍者传奇"
+CK4.TextColor3 = Color3.fromRGB(255, 255, 255)
+CK4.TextSize = 14.000
+CK4.MouseButton1Click:Connect(function()
 game:GetService("StarterGui"):SetCore("SendNotification",{ Title = "忍者传奇"; Text ="加载中…"; Duration = 4; });
 loadstring(game:HttpGet("https://raw.github.com/cikeV/-/main/%E5%BF%8D%E8%80%85.lua"))()
 end)
 
-SF5.Name = "SF"
-SF5.Parent = Main
-SF5.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
-SF5.BackgroundTransparency = 0.800
-SF5.BorderSizePixel = 0
-SF5.Position = UDim2.new(0.33, 0, 0.3, 0)
-SF5.Size = UDim2.new(0, 220, 0, 22)
-SF5.Font = Enum.Font.Arial
-SF5.Text = "战争大亨"
-SF5.TextColor3 = Color3.fromRGB(255, 255, 255)
-SF5.TextSize = 14.000
-SF5.MouseButton1Click:Connect(function()
+CK5.Name = "CK"
+CK5.Parent = Main
+CK5.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
+CK5.BackgroundTransparency = 0.800
+CK5.BorderSizePixel = 0
+CK5.Position = UDim2.new(0.33, 0, 0.3, 0)
+CK5.Size = UDim2.new(0, 220, 0, 22)
+CK5.Font = Enum.Font.Arial
+CK5.Text = "战争大亨"
+CK5.TextColor3 = Color3.fromRGB(255, 255, 255)
+CK5.TextSize = 14.000
+CK5.MouseButton1Click:Connect(function()
 game:GetService("StarterGui"):SetCore("SendNotification",{ Title = "战争大亨"; Text ="加载中…"; Duration = 4; });
 loadstring(game:HttpGet("https://raw.github.com/cikeV/-/main/%E6%88%98%E4%BA%89.lua"))()
 end)
 
-SF6.Name = "SF"
-SF6.Parent = Main
-SF6.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
-SF6.BackgroundTransparency = 0.800
-SF6.BorderSizePixel = 0
-SF6.Position = UDim2.new(0.67, 0, 0.3, 0)
-SF6.Size = UDim2.new(0, 220, 0, 22)
-SF6.Font = Enum.Font.Arial
-SF6.Text = "造船寻宝"
-SF6.TextColor3 = Color3.fromRGB(255, 255, 255)
-SF6.TextSize = 14.000
-SF6.MouseButton1Click:Connect(function()
+CK6.Name = "CK"
+CK6.Parent = Main
+CK6.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
+CK6.BackgroundTransparency = 0.800
+CK6.BorderSizePixel = 0
+CK6.Position = UDim2.new(0.67, 0, 0.3, 0)
+CK6.Size = UDim2.new(0, 220, 0, 22)
+CK6.Font = Enum.Font.Arial
+CK6.Text = "造船寻宝"
+CK6.TextColor3 = Color3.fromRGB(255, 255, 255)
+CK6.TextSize = 14.000
+CK6.MouseButton1Click:Connect(function()
 game:GetService("StarterGui"):SetCore("SendNotification",{ Title = " 造船寻宝"; Text ="加载中…"; Duration = 4; });
 loadstring(game:HttpGet("https://raw.github.com/cikeV/-/main/%E9%80%A0%E8%88%B9.lua"))()
 end)
 
-SF7.Name = "SF"
-SF7.Parent = Main
-SF7.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
-SF7.BackgroundTransparency = 0.800
-SF7.BorderSizePixel = 0
-SF7.Position = UDim2.new(0.000000002, 0, 0.38, 0)
-SF7.Size = UDim2.new(0, 220, 0, 22)
-SF7.Font = Enum.Font.Arial
-SF7.Text = "伐木大亨"
-SF7.TextColor3 = Color3.fromRGB(255, 255, 255)
-SF7.TextSize = 14.000
-SF7.MouseButton1Click:Connect(function()
+CK7.Name = "CK"
+CK7.Parent = Main
+CK7.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
+CK7.BackgroundTransparency = 0.800
+CK7.BorderSizePixel = 0
+CK7.Position = UDim2.new(0.000000002, 0, 0.38, 0)
+CK7.Size = UDim2.new(0, 220, 0, 22)
+CK7.Font = Enum.Font.Arial
+CK7.Text = "伐木大亨"
+CK7.TextColor3 = Color3.fromRGB(255, 255, 255)
+CK7.TextSize = 14.000
+CK7.MouseButton1Click:Connect(function()
 game:GetService("StarterGui"):SetCore("SendNotification",{ Title = " 伐木大亨"; Text ="加载中…"; Duration = 4; });
 loadstring(game:HttpGet("https://raw.github.com/cikeV/-/main/%E4%BC%90%E6%9C%A8.lua"))()
 end)
 
-SF8.Name = "SF"
-SF8.Parent = Main
-SF8.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
-SF8.BackgroundTransparency = 0.800
-SF8.BorderSizePixel = 0
-SF8.Position = UDim2.new(0.33, 0, 0.38, 0)
-SF8.Size = UDim2.new(0, 220, 0, 22)
-SF8.Font = Enum.Font.Arial
-SF8.Text = "俄亥俄州"
-SF8.TextColor3 = Color3.fromRGB(255, 255, 255)
-SF8.TextSize = 14.000
-SF8.MouseButton1Click:Connect(function()
+CK8.Name = "CK"
+CK8.Parent = Main
+CK8.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
+CK8.BackgroundTransparency = 0.800
+CK8.BorderSizePixel = 0
+CK8.Position = UDim2.new(0.33, 0, 0.38, 0)
+CK8.Size = UDim2.new(0, 220, 0, 22)
+CK8.Font = Enum.Font.Arial
+CK8.Text = "俄亥俄州"
+CK8.TextColor3 = Color3.fromRGB(255, 255, 255)
+CK8.TextSize = 14.000
+CK8.MouseButton1Click:Connect(function()
 game:GetService("StarterGui"):SetCore("SendNotification",{ Title = " 俄亥俄州"; Text ="维护中…"; Duration = 4; });
 
 end)
 
-SF9.Name = "SF"
-SF9.Parent = Main
-SF9.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
-SF9.BackgroundTransparency = 0.800
-SF9.BorderSizePixel = 0
-SF9.Position = UDim2.new(0.67, 0, 0.38, 0)
-SF9.Size = UDim2.new(0, 220, 0, 22)
-SF9.Font = Enum.Font.Arial
-SF9.Text = "一路向西"
-SF9.TextColor3 = Color3.fromRGB(255, 255, 255)
-SF9.TextSize = 14.000
-SF9.MouseButton1Click:Connect(function()
+CK9.Name = "CK"
+CK9.Parent = Main
+CK9.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
+CK9.BackgroundTransparency = 0.800
+CK9.BorderSizePixel = 0
+CK9.Position = UDim2.new(0.67, 0, 0.38, 0)
+CK9.Size = UDim2.new(0, 220, 0, 22)
+CK9.Font = Enum.Font.Arial
+CK9.Text = "一路向西"
+CK9.TextColor3 = Color3.fromRGB(255, 255, 255)
+CK9.TextSize = 14.000
+CK9.MouseButton1Click:Connect(function()
 game:GetService("StarterGui"):SetCore("SendNotification",{ Title = " 一路向西"; Text ="加载中…"; Duration = 4; });
 loadstring(game:HttpGet("https://raw.github.com/cikeV/-/main/%E4%B8%80%E8%B7%AF.lua"))()
 end)

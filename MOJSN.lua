ac4caf3335491664be8dbc8791f8352b425a984e
cikeV <151319654+cikeV@users.noreply.github.com>
2024-04-21 03:11:58 +0800

    Update MOJSN.lua

--- MOJSN.lua
+++ MOJSN.lua
@@ -1,3 +1,84 @@
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+local Frame = Instance.new("Frame")
+Frame.Size = UDim2.new(0, 400, 0, 300)
+Frame.Position = UDim2.new(0.5, 0, 0.5, 0)
+Frame.AnchorPoint = Vector2.new(0.5, 0.5)
+Frame.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
+Frame.BorderSizePixel = 0
+Frame.Active = true
+Frame.Draggable = true
+Frame.Parent = ScreenGui
+
+local FrameCorner = Instance.new("UICorner")
+FrameCorner.CornerRadius = UDim.new(0, 20)
+FrameCorner.Parent = Frame
+
+local Close = Instance.new("TextButton")
+Close.Size = UDim2.new(0, 40, 0, 40)
+Close.Position = UDim2.new(1, -40, 0, 0)
+Close.BackgroundTransparency = 1
+Close.Text = "×"
+Close.TextScaled = true
+Close.TextColor3 = Color3.fromRGB(150, 150, 150)
+Close.Parent = Frame
+Close.MouseButton1Click:Connect(function()
+   ScreenGui:Destroy()
+end)
+
+local Title = Instance.new("TextLabel")
+Title.Size = UDim2.new(1, 0, 0, 30)
+Title.Position = UDim2.new(0, 0, 0.05, 0)
+Title.Text = "◈刺客卡密校验系统◈"
+Title.TextSize = 18
+Title.TextColor3 = Color3.fromRGB(255, 255, 255)
+Title.BackgroundTransparency = 1
+Title.Parent = Frame
+
+local Instructions = Instance.new("TextLabel")
+Instructions.Size = UDim2.new(1, 0, 0, 30)
+Instructions.Position = UDim2.new(0, 0, 0.2, 0)
+Instructions.Text = "刺客Q群555251172\n卡密在群公告获取"
+Instructions.TextSize = 13
+Instructions.TextColor3 = Color3.fromRGB(150, 150, 150)
+Instructions.BackgroundTransparency = 1
+Instructions.Parent = Frame
+
+local TextBox = Instance.new("TextBox")
+TextBox.Size = UDim2.new(0.8, 0, 0.2, -10)
+TextBox.Position = UDim2.new(0.1, 0, 0.4, 0)
+TextBox.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+TextBox.PlaceholderText = "请输入你的卡密"
+TextBox.Text = ""
+TextBox.TextSize = 18
+TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextBox.Parent = Frame
+
+local TextBoxCorner = Instance.new("UICorner")
+TextBoxCorner.CornerRadius = UDim.new(0, 10)
+TextBoxCorner.Parent = TextBox
+
+local GetKey = Instance.new("TextButton")
+GetKey.Size = UDim2.new(0.6, 0, 0.15, 0)
+GetKey.Position = UDim2.new(0.2, 0, 0.7, 0)
+GetKey.BackgroundColor3 = Color3.fromRGB(54, 181, 0)
+GetKey.Text = "检查卡密"
+GetKey.TextSize = 18
+GetKey.TextColor3 = Color3.fromRGB(255, 255, 255)
+GetKey.Parent = Frame
+
+local GetKeyCorner = Instance.new("UICorner")
+GetKeyCorner.CornerRadius = UDim.new(0, 10)
+GetKeyCorner.Parent = GetKey
+
+GetKey.MouseButton1Click:Connect(function()
+    if TextBox.Text=="2024刺客牛逼" then
+    ScreenGui:Destroy()
+    game.StarterGui:SetCore("SendNotification",{Title="卡密系统",Text="卡密校验成功",Icon="rbxassetid://15512382151"})
+    game.StarterGui:SetCore("SendNotification",{Title="卡密系统",Text="正在初始",Icon="rbxassetid://15512382151"})
+        game.StarterGui:SetCore("SendNotification",{Title="卡密系统",Text="正在为您打开刺客免费版…",Icon="rbxassetid://15512382151"})
+
 msg = Instance.new("Message")
 msg.Parent = game.Workspace
 msg.Text = "-欢迎使用刺客免费版-"
@@ -103,3 +184,8 @@ msg:remove()
 wait(2)
 loadstring(game:HttpGet("https://raw.github.com/cikeV/-/main/%E9%80%9A%E7%94%A8.lua"))()
 end
+    else
+    game.StarterGui:SetCore("SendNotification",{Title="卡密系统",Text="卡密错误",Icon="rbxassetid://15512382151"})
+        --错误
+    end
+end)
